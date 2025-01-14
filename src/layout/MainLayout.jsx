7c8ebc0fa8b7d08f856dc589e047c70cb558097e
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-01-14 17:21:46 +0600

    footer design completed

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export const MainLayout = () => {
 	return (
 		<div>
-			<Navbar/>
-			<Outlet />
-			<h2>Footer</h2>
+			<Navbar />
+			<main className="min-h-[calc(100vh-306px)]">
+				<Outlet />
+			</main>
+			<Footer />
 		</div>
 	);
 };
