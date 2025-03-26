b28ebbaf688ea74d52fb53f2b8641f58c7e58e24
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-03-26 14:37:54 +0600

    fix: cors url

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,8 +5,8 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_API_KEY,
-	authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+	apiKey: import.meta.env.VITE_API,
+	authDomain: import.meta.env.VITE_DOMAIN,
 	projectId: import.meta.env.VITE_PROJECT_ID,
 	storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
 	messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
